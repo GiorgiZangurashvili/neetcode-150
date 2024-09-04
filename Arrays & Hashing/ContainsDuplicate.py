1a5d03a7cac77e747ed3d5497aeb1bd6aaf958a0
GiorgiZangurashvili <158226767+GiorgiZangurashvili@users.noreply.github.com>
2024-09-04 18:49:22 +0400

    Update ContainsDuplicate.py

--- Arrays & Hashing/ContainsDuplicate.py
+++ Arrays & Hashing/ContainsDuplicate.py
@@ -1,3 +1,8 @@
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         hashset = set()
